2fd684a89939a66293d024567c827e3f883be3ee
matthewnegrido <matthewnegrido@gmail.com>
2026-06-05 19:01:25 +0800

    defining setters

--- Pet_Class.py
+++ Pet_Class.py
@@ -12,3 +12,12 @@ class Pet:
 
     def set_age(self, age):
         self.__age = int(age)
+
+    def get_name(self):
+        return self.__name
+
+    def get_animal_type(self):
+        return self.__animal_type
+
+    def get_age(self):
+        return self.__age
